707665741a9da04fddab2d065f58c1f24346011f
Nekohy <qaq3254865169@gmail.com>
2024-11-04 02:05:14 +0800

    自用，测试，去除o1判断

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -194,7 +194,7 @@ export class ChatGPTApi implements LLMApi {
     let requestPayload: RequestPayload | DalleRequestPayload;
 
     const isDalle3 = _isDalle3(options.config.model);
-    const isO1 = False;
+    const isO1 = false;
     if (isDalle3) {
       const prompt = getMessageTextContent(
         options.messages.slice(-1)?.pop() as any,
